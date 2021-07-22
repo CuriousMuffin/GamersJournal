24883ee51c9cfd1f4fc6abd52ec4a336707ca3b5
Nicola Calvanese <nicola.c91@gmail.com>
2021-07-22 17:59:41 +0200

    piccole migliorie estetiche admin

--- UniversoGame/src/main/resources/static/js/cms.js
+++ UniversoGame/src/main/resources/static/js/cms.js
@@ -17,19 +17,27 @@ function getContentList() {
       if (isPublished(res[i].bozza)) {
         $(`
       <tr id="recensione" data-id='${res[i].id}'>
+			<td>RECENSIONE</td>
 			<td>${res[i].titolo}</td>
 			<td>${res[i].dataPubblicazione}</td>
 			<td>${res[i].utente.username}</td>
-			<td><button>Modifica</button> <button>Elimina</button> <button>Bozza</button></td>
+			<td><button><i class="far fa-fw fa-edit"></i></button>
+			 	<button><i class="far fa-fw fa-trash-alt"></i>
+				</button> <button>Bozza</button>
+			</td>
       </tr>
       `).appendTo($(".content-list"));
       } else {
 		$(`
       <tr id="recensione" data-id='${res[i].id}'>
+			<td>RECENSIONE</td>
 			<td>${res[i].titolo}</td>
 			<td>${res[i].dataPubblicazione}</td>
 			<td>${res[i].utente.username}</td>
-			<td><button>Modifica</button> <button>Elimina</button> <button>Pubblica</button></td>
+			<td><button><i class="far fa-fw fa-edit"></i></button>
+			 	<button><i class="far fa-fw fa-trash-alt"></i>
+				</button> <button>Pubblica</button>
+			</td>
       </tr>
       `).appendTo($(".draft-list"));
 		}
@@ -43,19 +51,27 @@ function getContentList() {
       if (isPublished(res[i].bozza)) {
         $(`
       <tr id="notizia" data-id='${res[i].id}'>
+			<td>NOTIZIA</td>
 			<td>${res[i].titolo}</td>
 			<td>${res[i].dataPubblicazione}</td>
 			<td>${res[i].utente.username}</td>
-			<td><button>Modifica</button> <button>Elimina</button> <button>Bozza</button></td>
+			<td><button><i class="far fa-fw fa-edit"></i></button>
+			 	<button><i class="far fa-fw fa-trash-alt"></i>
+				</button> <button>Bozza</button>
+			</td>
       </tr>
       `).appendTo($(".content-list"));
       } else {
 		$(`
       <tr id="notizia" data-id='${res[i].id}'>
+			<td>NOTIZIA</td>
 			<td>${res[i].titolo}</td>
 			<td>${res[i].dataPubblicazione}</td>
 			<td>${res[i].utente.username}</td>
-			<td><button>Modifica</button> <button>Elimina</button> <button>Pubblica</button></td>
+			<td><button><i class="far fa-fw fa-edit"></i></button>
+			 	<button><i class="far fa-fw fa-trash-alt"></i>
+				</button> <button>Pubblica</button>
+			</td>
       </tr>
       `).appendTo($(".draft-list"));
 		}
